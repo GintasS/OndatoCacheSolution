6885bca0276b8978a8e66ad886cf68e9df03581e
JauniusPinelis <JauniusPinelis@gmail.com>
2021-05-21 14:05:10 +0300

    More Integration tests

--- tests/OndatoCacheSolution.IntegrationTests/CacheControllerTests.cs
+++ tests/OndatoCacheSolution.IntegrationTests/CacheControllerTests.cs
@@ -7,7 +7,6 @@ using OndatoCacheSolution.WebApi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Xunit;
@@ -45,6 +44,34 @@ namespace OndatoCacheSolution.IntegrationTests
         }
 
         [Fact]
+        public async Task Create_GivenExistingKey_ValueGetsOverwritten()
+        {
+            var client = _factory.CreateClient();
+            var key = "key";
+
+            var dto = _fixture.Build<CreateListCacheItemDto>()
+                .With(c => c.Key, key)
+                .With(c => c.Value, _fixture.CreateMany<object>(3).ToList()
+                ).Create();
+
+            var response = await client.PostAsJsonAsync("/Cache", dto);
+            response.EnsureSuccessStatusCode();
+
+            var newDto = _fixture.Build<CreateListCacheItemDto>()
+               .With(c => c.Key, key)
+               .With(c => c.Value, _fixture.CreateMany<object>(5).ToList()
+               ).Create();
+
+            response = await client.GetAsync($"/Cache/{newDto.Key}");
+            response.EnsureSuccessStatusCode();
+
+            var receivedStringValue = await response.Content.ReadAsStringAsync();
+            var receivedValue = JsonConvert.DeserializeObject<List<object>>(receivedStringValue);
+
+            newDto.Value.Count().Should().Be(receivedValue.Count());
+        }
+
+        [Fact]
         public async Task Delete_DeletingCreatedItem_ItemGetsDeleted()
         {
             var client = _factory.CreateClient();
@@ -59,6 +86,29 @@ namespace OndatoCacheSolution.IntegrationTests
 
             response = await client.GetAsync($"/Cache/{dto.Key}");
             response.StatusCode.Should().Be(404);
+        }
+
+        [Fact]
+        public async Task Put_GivenNewItem_DataGetsAppended()
+        {
+            var client = _factory.CreateClient();
+
+            var dto = _fixture.Build<CreateListCacheItemDto>()
+                .With(c => c.Value, _fixture.CreateMany<object>().ToList()).Create();
+
+            var response = await client.PostAsJsonAsync("/Cache", dto);
+            response.EnsureSuccessStatusCode();
+
+            response = await client.PutAsJsonAsync("/Cache", dto);
+            response.EnsureSuccessStatusCode();
+
+            response = await client.GetAsync($"/Cache/{dto.Key}");
+            response.EnsureSuccessStatusCode();
+
+            var receivedStringValue = await response.Content.ReadAsStringAsync();
+            var receivedValue = JsonConvert.DeserializeObject<List<object>>(receivedStringValue);
+
+            (dto.Value.Count() * 2).Should().Be(receivedValue.Count());
         }
     }
 }
